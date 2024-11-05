8d8355d966b7c1847fcef50b618fa53eeba5fa0c
Gongas <goncalo.mesteves03@gmail.com>
2024-11-05 13:01:49 +0000

    Fixed typo

--- SPX Stock Forecasting and Optimization/Utils/__init__.py
+++ SPX Stock Forecasting and Optimization/Utils/__init__.py
@@ -1,4 +1,4 @@
 # Defining which submodules to import when using from <package> import *
 __all__ = ["load_config"]
 
-from .Configuration import (load_config)
+from .Configuration import (loadConfig)
