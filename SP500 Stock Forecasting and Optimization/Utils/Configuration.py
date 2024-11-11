e2e7770bdf1fe501982466063f7010f16c119829
Gongas <goncalo.mesteves03@gmail.com>
2024-11-11 09:18:17 +0000

    Updated config to the correct timestamps to extract the data from

--- SP500 Stock Forecasting and Optimization/Utils/Configuration.py
+++ SP500 Stock Forecasting and Optimization/Utils/Configuration.py
@@ -7,9 +7,9 @@ def loadConfig() -> dict:
     """
     
     return {'N':3,                      # Number of previous data to be considered when predicting a next value
-            'max_period':True,         # Flag to determine whether or not to consider all the historical data, i.e, all the information available 
-            'start_date':'2022-12-04',  # Start date of the data to be considered
-            'end_date':'2024-08-12',    #  End date of the data to be considered
+            'max_period':False,         # Flag to determine whether or not to consider all the historical data, i.e, all the information available 
+            'start_date':'2010-01-01',  # Start date of the data to be considered
+            'end_date':'2023-12-31',    # End date of the data to be considered
             'save_plot':False}          # Flag that decides whether or not to save the Final graph of the Notebook
 
 
