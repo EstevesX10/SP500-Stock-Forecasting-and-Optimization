bcc4715a67eb1af8d12ff8a03d135865d6c66ac2
Gongas <goncalo.mesteves03@gmail.com>
2024-12-02 23:26:18 +0000

    Updated with a different color pallete

--- SP500 Stock Forecasting and Optimization/ModelDevelopmentAndEvaluation/DataVisualization.py
+++ SP500 Stock Forecasting and Optimization/ModelDevelopmentAndEvaluation/DataVisualization.py
@@ -1,5 +1,27 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib.colors as mcolors
+import seaborn as sns
+
+def pastelizeColor(c:tuple, weight:float=None) -> np.ndarray:
+    """
+    # Description
+        -> Lightens the input color by mixing it with white, producing a pastel effect.
+    -----------------------------------------------------------------------------------
+    := param: c - Original color.
+    := param: weight - Amount of white to mix (0 = full color, 1 = full white).
+    """
+
+    # Set a default weight
+    weight = 0.5 if weight is None else weight
+
+    # Initialize a array with the white color values to help create the pastel version of the given color
+    white = np.array([1, 1, 1])
+
+    # Returns a tuple with the values for the pastel version of the color provided
+    return mcolors.to_rgba((np.array(mcolors.to_rgb(c)) * (1 - weight) + white * weight))
 
 def plotStocksRawResults(stockSymbol:str, pathsConfig:dict) -> None:
     """
@@ -27,16 +49,20 @@ def plotStocksRawResults(stockSymbol:str, pathsConfig:dict) -> None:
     # Get the MAPE columns
     mapeColumns = [col for col in stockResults.columns if 'Error' in col and '(%)' in col]
 
+    # Create a color map
+    cmap = cm.get_cmap('RdYlGn_r')
+    colors = [pastelizeColor(cmap(i / (len(predictionColumns) - 1))) for i in range(len(predictionColumns))]
+
     # Define a Figure
     fig, axes = plt.subplots(1, 3, figsize=(12, 4), sharex=True)
 
     # Plot the Model Predictions
-    for modelPredictionColumn in predictionColumns:
+    for idx, modelPredictionColumn in enumerate(predictionColumns):
         # Get the Model Name
         modelName = modelPredictionColumn.split('[')[1].split(']')[0]
 
         # Plot the Model Prediction
-        axes[0].plot(stockResults['Date'], stockResults[modelPredictionColumn], label=modelName, marker='o')
+        axes[0].plot(stockResults['Date'], stockResults[modelPredictionColumn], color=colors[idx], label=modelName, marker='o')
 
     # Plot the Target Value
     axes[0].plot(stockResults['Date'], stockResults['Target'], label='Real Value', marker='x')
@@ -47,15 +73,15 @@ def plotStocksRawResults(stockSymbol:str, pathsConfig:dict) -> None:
     axes[0].set_xticklabels(stockResults['Date'], fontsize=7, rotation=45, ha='center', va='top')
     axes[0].set_ylabel('Stock Value')
     axes[0].legend()
-    axes[0].grid(True)
+    axes[0].grid(color='lightgray', linestyle='--', linewidth=0.5, alpha=0.7)
 
     # Plot the Mean Absolute Errors
-    for maeColumn in maeColumns:
+    for idx, maeColumn in enumerate(maeColumns):
         # Get the Model Name
         modelName = maeColumn.split('[')[1].split(']')[0]
 
         # Plot the Model Mean Absolute Error
-        axes[1].plot(stockResults['Date'], stockResults[maeColumn], label=modelName, marker='s')
+        axes[1].plot(stockResults['Date'], stockResults[maeColumn], color=colors[idx], label=modelName, marker='.')
     
     # Plot the title and the axis names
     axes[1].set_title('Mean Absolute Error')
@@ -63,15 +89,15 @@ def plotStocksRawResults(stockSymbol:str, pathsConfig:dict) -> None:
     axes[1].set_xticklabels(stockResults['Date'], fontsize=7, rotation=45, ha='center', va='top')
     axes[1].set_ylabel('Error')
     axes[1].legend()
-    axes[1].grid(True)
+    axes[1].grid(color='lightgray', linestyle='--', linewidth=0.5, alpha=0.7)
 
     # Plot the Mean Absolute Percentage Error
-    for mapeColumn in mapeColumns:
+    for idx, mapeColumn in enumerate(mapeColumns):
         # Get the Model Name
         modelName = mapeColumn.split('[')[1].split(']')[0]
 
         # Plot the Model Mean Absolute Percentage Error
-        axes[2].plot(stockResults['Date'], stockResults[mapeColumn], label=modelName, marker='s')
+        axes[2].plot(stockResults['Date'], stockResults[mapeColumn], color=colors[idx], label=modelName, marker='.')
     
     # Set the title and the Axis labeling
     axes[2].set_title('Mean Absolute Percentage Error')
@@ -79,7 +105,7 @@ def plotStocksRawResults(stockSymbol:str, pathsConfig:dict) -> None:
     axes[2].set_xticklabels(stockResults['Date'], fontsize=7, rotation=45, ha='center', va='top')
     axes[2].set_ylabel('Error')
     axes[2].legend()
-    axes[2].grid(True)
+    axes[2].grid(color='lightgray', linestyle='--', linewidth=0.5, alpha=0.7)
 
     # Adjust layout
     plt.tight_layout()
